35fb9b7d2ac358d3e584d6f05a74f0c88c6d4d25
Michelle <nyamburamichelle400@gmail.com>
2024-05-27 17:18:47 +0300

    complrtes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def match(self, possible_anagrams):
+        matches = []
+        for anagram in possible_anagrams:
+            if anagram.lower() != self.word and ''.join(sorted(anagram.lower())) == self.sorted_word:
+                matches.append(anagram)
+        return matches
